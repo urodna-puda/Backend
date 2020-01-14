58272285a174f3285b43da522ea67cde0c7a7159
Michal Krejčí <krejcar25@blep.cz>
2020-01-14 23:12:02 +0100

    Create Models

--- backend/pos/posapp/models.py
+++ backend/pos/posapp/models.py
@@ -1,3 +1,109 @@
+from uuid import uuid4
+
 from django.db import models
+from django_countries.fields import CountryField
+
 
 # Create your models here.
+
+class Product(models.Model):
+    id = models.UUIDField(primary_key=True, null=False, editable=False, default=uuid4)
+    name = models.CharField(max_length=1024, null=False)
+    price = models.DecimalField(max_digits=15, decimal_places=3)
+
+
+class UnitGroup(models.Model):
+    id = models.UUIDField(primary_key=True, null=False, editable=False, default=uuid4)
+    name = models.CharField(max_length=256, null=False)
+    symbol = models.CharField(max_length=16, null=False)
+
+
+class Unit(models.Model):
+    id = models.UUIDField(primary_key=True, null=False, editable=False, default=uuid4)
+    group = models.ForeignKey(UnitGroup, on_delete=models.PROTECT)
+    name = models.CharField(max_length=256, null=False)
+    symbol = models.CharField(max_length=16, null=False)
+
+
+class Item(models.Model):
+    id = models.UUIDField(primary_key=True, null=False, editable=False, default=uuid4)
+    name = models.CharField(max_length=1024, null=False)
+    unitGroup = models.ForeignKey(UnitGroup, on_delete=models.PROTECT)
+
+
+class ItemInProduct(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    amount = models.FloatField()
+
+
+class Ticket(models.Model):
+    OPEN = 'O'
+    PAID = 'P'
+    ORDER_STATES = [
+        (OPEN, "Open"),
+        (PAID, "Paid"),
+    ]
+    id = models.UUIDField(primary_key=True, null=False, editable=False, default=uuid4)
+    name = models.CharField(max_length=256, null=False)
+    products = models.ManyToManyField(Product, through="ProductInTicket")
+    state = models.CharField(max_length=1, choices=ORDER_STATES, default=OPEN)
+    openedAt = models.DateTimeField(auto_now_add=True)
+    closedAt = models.DateTimeField(null=True, blank=True)
+
+
+class ProductInTicket(models.Model):
+    ORDERED = 'O'
+    PREPARING = 'P'
+    TO_SERVE = 'T'
+    SERVED = 'S'
+    SERVING_STATES = [
+        (ORDERED, "Ordered"),
+        (PREPARING, "Being prepared"),
+        (TO_SERVE, "To be served"),
+        (SERVED, "Served"),
+    ]
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE)
+    state = models.CharField(max_length=1, choices=SERVING_STATES, default=ORDERED)
+    price = models.DecimalField(max_digits=15, decimal_places=3)
+    orderedAt = models.DateTimeField(auto_now_add=True)
+    preparingAt = models.DateTimeField(null=True, blank=True)
+    preparedAt = models.DateTimeField(null=True, blank=True)
+    servedAt = models.DateTimeField(null=True, blank=True)
+
+
+class Currency(models.Model):
+    name = models.CharField(max_length=1024, null=False)
+    code = models.CharField(max_length=3, null=True)
+    symbol = models.CharField(max_length=8, null=True)
+    countries = CountryField(multiple=True)
+    subunit = models.CharField(max_length=1024, null=False)
+    ratio = models.FloatField(default=1)
+    enabled = models.BooleanField(default=False)
+
+
+class PaymentMethod(models.Model):
+    id = models.UUIDField(primary_key=True, null=False, editable=False, default=uuid4)
+    name = models.CharField(max_length=1024, null=False)
+    currency = models.ForeignKey(Currency, on_delete=models.PROTECT, limit_choices_to={"enabled": True})
+
+
+class Order(models.Model):
+    CREATED = 'C'
+    PAID = 'P'
+    ORDER_STATES = [
+        (CREATED, "Created"),
+        (PAID, "Paid"),
+    ]
+    id = models.UUIDField(primary_key=True, null=False, editable=False, default=uuid4)
+    ticket = models.OneToOneField(Ticket, on_delete=models.PROTECT, related_name="order")
+    state = models.CharField(max_length=1, choices=ORDER_STATES, default=CREATED)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    payments = models.ManyToManyField(PaymentMethod, through="PaymentInOrder")
+
+
+class PaymentInOrder(models.Model):
+    method = models.ForeignKey(PaymentMethod, on_delete=models.PROTECT)
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    amount = models.DecimalField(max_digits=15, decimal_places=3)
