1636f095c54518290f3a5c008a3ce92d1fe1d4f2
Michal Krejčí <krejcar25@blep.cz>
2020-01-21 16:45:02 +0100

    Add str methods to models

--- pos/posapp/models.py
+++ pos/posapp/models.py
@@ -11,11 +11,17 @@ class Product(models.Model):
     name = models.CharField(max_length=1024, null=False)
     price = models.DecimalField(max_digits=15, decimal_places=3)
 
+    def __str__(self):
+        return self.name
+
 
 class UnitGroup(models.Model):
     id = models.UUIDField(primary_key=True, null=False, editable=False, default=uuid4)
     name = models.CharField(max_length=256, null=False)
     symbol = models.CharField(max_length=16, null=False)
+
+    def __str__(self):
+        return self.name
 
 
 class Unit(models.Model):
@@ -25,17 +31,26 @@ class Unit(models.Model):
     symbol = models.CharField(max_length=16, null=False)
     ratio = models.FloatField()
 
+    def __str__(self):
+        return f"{self.name} ({self.group})"
+
 
 class Item(models.Model):
     id = models.UUIDField(primary_key=True, null=False, editable=False, default=uuid4)
     name = models.CharField(max_length=1024, null=False)
     unitGroup = models.ForeignKey(UnitGroup, on_delete=models.PROTECT)
 
+    def __str__(self):
+        return self.name
+
 
 class ItemInProduct(models.Model):
     item = models.ForeignKey(Item, on_delete=models.PROTECT)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     amount = models.FloatField()
+
+    def __str__(self):
+        return f"{self.item} in {self.product}"
 
 
 class Tab(models.Model):
@@ -51,6 +66,16 @@ class Tab(models.Model):
     state = models.CharField(max_length=1, choices=ORDER_STATES, default=OPEN)
     openedAt = models.DateTimeField(auto_now_add=True)
     closedAt = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def total(self):
+        sum = 0
+        for product in ProductInTab.objects.filter(tab=self):
+            sum += product.price
+        return sum
 
 
 class ProductInTab(models.Model):
@@ -74,6 +99,9 @@ class ProductInTab(models.Model):
     servedAt = models.DateTimeField(null=True, blank=True)
     note = models.TextField(null=True)
 
+    def __str__(self):
+        return f"{self.product} in {self.tab}"
+
 
 class Currency(models.Model):
     name = models.CharField(max_length=1024, null=False)
@@ -84,11 +112,17 @@ class Currency(models.Model):
     ratio = models.FloatField(default=1)
     enabled = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.name
+
 
 class PaymentMethod(models.Model):
     id = models.UUIDField(primary_key=True, null=False, editable=False, default=uuid4)
     name = models.CharField(max_length=1024, null=False)
     currency = models.ForeignKey(Currency, on_delete=models.PROTECT, limit_choices_to={"enabled": True})
+
+    def __str__(self):
+        return self.name
 
 
 class Order(models.Model):
@@ -104,8 +138,14 @@ class Order(models.Model):
     createdAt = models.DateTimeField(auto_now_add=True)
     payments = models.ManyToManyField(PaymentMethod, through="PaymentInOrder")
 
+    def __str__(self):
+        return f"Order of tab {self.tab}"
+
 
 class PaymentInOrder(models.Model):
     method = models.ForeignKey(PaymentMethod, on_delete=models.PROTECT)
     order = models.ForeignKey(Order, on_delete=models.PROTECT)
     amount = models.DecimalField(max_digits=15, decimal_places=3)
+
+    def __str__(self):
+        return f"Payment of {self.method.currency.symbol}{self.amount} via {self.method} for {self.order}"
