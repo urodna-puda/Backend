85643fe6f9669dafc9ef9090944da86f8780d735
Michal Krejčí <krejcar25@blep.cz>
2020-07-01 12:12:08 +0200

    Generalised transition URL converter
    And used the generalisation to create a converter for Membership status

--- pos/posapp/converters.py
+++ pos/posapp/converters.py
@@ -1,18 +1,27 @@
-from posapp.models import Expense
+from typing import Type
+
+from posapp.models import HasActionsMixin, Expense, Member
 
 
-class ExpenseTransitionConverter:
-    actions = Expense.list_actions()
-    regex = f'(?:{"|".join(actions)})'
+def generate_transition_converter(cls: Type[HasActionsMixin], group: str = "state"):
+    class TransitionConverter:
+        actions = cls.list_actions(group)
+        regex = f'(?:{"|".join(actions)})'
 
-    def to_python(self, value):
-        if value in self.actions:
-            return str(value)
-        else:
-            raise ValueError("value must be one of " + str(self.actions))
+        def to_python(self, value):
+            if value in self.actions:
+                return str(value)
+            else:
+                raise ValueError("value must be one of " + str(self.actions))
 
-    def to_url(self, value):
-        if value in self.actions:
-            return str(value)
-        else:
-            raise ValueError("value must be one of " + str(self.actions))
+        def to_url(self, value):
+            if value in self.actions:
+                return str(value)
+            else:
+                raise ValueError("value must be one of " + str(self.actions))
+
+    return TransitionConverter
+
+
+ExpenseTransitionConverter = generate_transition_converter(Expense)
+MembershipTransitionConverter = generate_transition_converter(Member, "membership_status")
