c7696aebfe5df9286cf972d9510fb7d80b12637d
Michal Krejčí <krejcar25@blep.cz>
2020-07-28 16:23:40 +0200

    Moved posapp url to /posapp

--- pos/puda/urls.py
+++ pos/puda/urls.py
@@ -14,13 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path, include
 
-from posapp import urls, apiurls
-
 urlpatterns = [
+    path('', lambda *args, **kwargs: redirect('/posapp')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('', include(urls)),
-    path('api/1/', include(apiurls)),
+    path('posapp/', include('posapp.urls')),
+    path('api/1/', include('posapp.apiurls')),
 ]
